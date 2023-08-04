9ee8bd39c0ae349e49d676279af3348bd49217ff
chalee7555 <arthit.scs57@ubru.ac.th>
2023-08-04 11:52:41 +0700

    ddd

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,7 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  mode: "universal", target: "static",
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
